b58a0c3e3c243a4fddcce5beec87c238aa83c20a
江聪 <keepjsls@163.com>
2022-05-20 13:53:11 +0800

    link-list

---
+++ link-list/奇偶链表.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var oddEvenList = function(head) {
+    if (head === null) {
+        return head;
+    }
+    let evenHead = head.next;
+    let odd = head, even = evenHead;
+    while (even !== null && even.next !== null) {
+        odd.next = even.next;
+        odd = odd.next;
+        even.next = odd.next;
+        even = even.next;
+    }
+    odd.next = evenHead;
+    return head;
+};
+
+// [1,2,3,4,5]  =>  [1,3,5,2,4]
+
---
+++ link-list/相交链表.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+ var getIntersectionNode = function(headA, headB) {
+    if (headA === null || headB === null) {
+        return null;
+    }
+    let pA = headA, pB = headB;
+    while (pA !== pB) {
+        pA = pA === null ? headB : pA.next;
+        pB = pB === null ? headA : pB.next;
+    }
+    return pA;
+};
+
+// 8
+// [4,1,8,4,5]
+// [5,6,1,8,4,5]   =>   Intersected at '8'
+// 2
+// 3
