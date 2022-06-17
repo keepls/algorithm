fedcfad57de7440689a43ff899b38dd2e3e32b14
江聪 <keepjsls@163.com>
2022-06-17 13:50:05 +0800

    m

---
+++ string/反转字符窜中的单词3.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var reverseWords = function(s) {
+    const ret = [];
+    const length = s.length;
+    let i = 0;
+    while (i < length) {
+        let start = i;
+        while (i < length && s.charAt(i) != ' ') {
+            i++;
+        }
+        for (let p = start; p < i; p++) {
+            ret.push(s.charAt(start + i - 1 - p));
+        }
+        while (i < length && s.charAt(i) == ' ') {
+            i++;
+            ret.push(' ');
+        }
+    }
+    return ret.join('');
+};
+// "Let's take LeetCode contest" => "s'teL ekat edoCteeL tsetnoc"
