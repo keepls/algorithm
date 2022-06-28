9da45303ef64be544ec42868d1927d1a114c5856
江聪 <keepjsls@163.com>
2022-06-28 15:20:58 +0800

    m

---
+++ string/寻找重复数.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+ var findDuplicate = function(nums) {
+    const n = nums.length;
+    let l = 1, r = n - 1, ans = -1;
+    while (l <= r) {
+        let mid = (l + r) >> 1;
+        let cnt = 0;
+        for (let i = 0; i < n; ++i) {
+            cnt += nums[i] <= mid;
+        }
+        if (cnt <= mid) {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+            ans = mid;
+        }
+    }
+    return ans;
+};
+// [1,3,4,2,2] => 2
